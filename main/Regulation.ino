69869d281b267e2baa66186e6e5da502fb43de24
Kris Raich <Krispin.Raich@fh-kufstein.ac.at>
2018-02-21 13:36:52 +0100

    added regulation and theroy docs

---
+++ main/Regulation.ino
@@ -0,0 +1,13 @@
+/*
+ * Temperture regulation
+ */
+
+ void setup_regulation(){
+  if (DEBUG) Serial.println("Init Regulation"); 
+
+  pinMode(CHANNEL_1, OUTPUT);
+  pinMode(CHANNEL_2, OUTPUT);
+
+  digitalWrite(CHANNEL_1, LOW);
+  digitalWrite(CHANNEL_2, LOW);
+}
